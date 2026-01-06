23099e7ce9d655c59997976f4028bc589296d837
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2026-01-06 14:44:57 +0100

    init projet, npm, prettier

---
+++ padre-ginos/src/App.js
@@ -0,0 +1,37 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement(
+      "h1",
+      { style: { color: "red" }, id: "title" },
+      "Padre Gino's",
+    ),
+
+    React.createElement(Pizza, {
+      name: "The pepperoni pizza",
+      description: "Tomatoe, mozarella, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "American pizza",
+      description: "Tomatoe, mozarella, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawai pizza",
+      description: "Tomatoe, mozarella, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "Mortadella pizza",
+      description: "Tomatoe, mozarella, mortadella",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
